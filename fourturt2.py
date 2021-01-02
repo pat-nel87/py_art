bb2f142f115a922aa50a191d667e834656685bf3
pat-nel87 <patnelson2022@gmail.com>
2021-01-02 16:28:49 -0500

    improved fourturt2.py

--- fourturt2.py
+++ fourturt2.py
@@ -1,17 +1,17 @@
 import turtle
 
-turtle.title("A Tale of Four Turtles")
+turtle.title("Advanced Turtlenometry")
 
 window = turtle.Screen()
 window.setup(width=800, height=600, startx=10, starty=0.5)
-turtle.bgcolor("yellow")
+turtle.bgcolor("black")
 
 boole = turtle.Turtle()
 boole.color("red")
 boole.shape("turtle")
 
 pascal = turtle.Turtle()
-pascal.color("blue")
+pascal.color("yellow")
 pascal.shape("turtle")
 
 feynman = turtle.Turtle()
@@ -19,7 +19,7 @@ feynman.color("green")
 feynman.shape("turtle")
 
 mandelbrot = turtle.Turtle()
-mandelbrot.color("purple")
+mandelbrot.color("orange")
 mandelbrot.shape("turtle")
 
 boole.pendown()
@@ -30,15 +30,18 @@ mandelbrot.pendown()
 for x in range (1,1000000000):
     if x % 2 == 0:
         boole.setheading(x*70)
-        boole.forward(x+1)
+        boole.forward(x+1) 
         boole.speed(5*x)
-        if boole.pos() == pascal.pos():
-            boole.backward(x*2)
+
         feynman.setheading(x*120)
         feynman.speed(3/x)
+        feynman.circle(x)
         feynman.backward(x-1)
     elif x % 3 == 0:
         pascal.setheading(x*33)
         pascal.forward(x+1)
         mandelbrot.setheading(x*9)
+        mandelbrot.speed(x*3.14)
+        mandelbrot.circle(x*3.14)
+
         mandelbrot.backward(x-1)
